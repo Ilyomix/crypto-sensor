03a1859c6c6d2a620c06e0380cf179421d94df8f
El-Hadj Ilyes Abd-Lillah <ilyes1204@gmail.com>
2025-01-02 02:11:30 +0100

    (googleAdSense): adding adds

--- src/app/components/analytics/GoogleAnalytics.tsx
+++ src/app/components/analytics/GoogleAnalytics.tsx
@@ -16,6 +16,21 @@ export default function GoogleAnalytics() {
           gtag('config', 'G-GNMSSCC4PZ');
         `}
       </Script>
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5274201910411416"
+        crossOrigin="anonymous"
+      />
+      <ins className="adsbygoogle"
+        style={{ display: 'block' }}
+        data-ad-client="ca-pub-5274201910411416"
+        data-ad-slot="6504159204"
+        data-ad-format="auto"
+        data-full-width-responsive="true">  
+      </ins>
+      <Script id="adsbygoogle-init">
+          (adsbygoogle = window.adsbygoogle || []).push({});
+      </Script>
     </>
   )
 }
