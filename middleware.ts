5af1b97b8180564a3f8637e2f6c2ff2d12df88fa
El-Hadj Ilyes Abd-Lillah <ilyes1204@gmail.com>
2025-01-02 03:12:40 +0100

    (ads): adding adSense

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export function middleware(request) {
-  const url = request.nextUrl.clone();
-  if (url.pathname === '/ads.txt') {
-    return NextResponse.next(); // Allow access to ads.txt
-  }
-  return NextResponse.next();
-}
