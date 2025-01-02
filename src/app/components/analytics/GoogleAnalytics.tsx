769e5fa911b04f6bb8e998cddb88d866e22defa2
El-Hadj Ilyes Abd-Lillah <ilyes1204@gmail.com>
2025-01-02 02:14:34 +0100

    (googleAdSense): adding ads

--- src/app/components/analytics/GoogleAnalytics.tsx
+++ src/app/components/analytics/GoogleAnalytics.tsx
@@ -3,6 +3,7 @@ import Script from 'next/script'
 export default function GoogleAnalytics() {
   return (
     <>
+      <meta name="google-adsense-account" content="ca-pub-5274201910411416"></meta>
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=G-GNMSSCC4PZ"
         strategy="afterInteractive"
@@ -15,21 +16,6 @@ export default function GoogleAnalytics() {
 
           gtag('config', 'G-GNMSSCC4PZ');
         `}
-      </Script>
-      <Script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5274201910411416"
-        crossOrigin="anonymous"
-      />
-      <ins className="adsbygoogle"
-        style={{ display: 'block' }}
-        data-ad-client="ca-pub-5274201910411416"
-        data-ad-slot="6504159204"
-        data-ad-format="auto"
-        data-full-width-responsive="true">  
-      </ins>
-      <Script id="adsbygoogle-init">
-          (adsbygoogle = window.adsbygoogle || []).push({});
       </Script>
     </>
   )
