7fc33ae0db32dc5f44cc9fb1fe43d953deff3192
El-Hadj Ilyes Abd-Lillah <ilyes1204@gmail.com>
2025-01-11 12:02:51 +0100

    (ads): adding ads

--- src/app/components/analytics/GoogleAnalytics.tsx
+++ src/app/components/analytics/GoogleAnalytics.tsx
@@ -3,7 +3,6 @@ import Script from 'next/script'
 export default function GoogleAnalytics() {
   return (
     <>
-      <meta name="google-adsense-account" content="ca-pub-5274201910411416"></meta>
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=G-GNMSSCC4PZ"
         strategy="afterInteractive"
