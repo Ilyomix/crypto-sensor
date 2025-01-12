f32addbde94ad7dc822c987ca8145efbe9e3852c
Ilyes Abd-Lillah <ilyes1204@gmail.com>
2025-01-12 02:06:06 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,18 +5,6 @@ import { MetricsDashboard } from "@/src/app/components/dashboard/metrics-dashboa
 import { SkeletonLoader } from "@/src/app/components/dashboard/skeleton-loader"
 
 export default function Page() {
-   useEffect(() => {
-      if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('/sw.js')
-          .then((registration) => {
-            console.log('Service Worker registered:', registration);
-          })
-          .catch((error) => {
-            console.error('Service Worker registration failed:', error);
-          });
-      }
-    }, []);
-  
   return (
     <Suspense fallback={<SkeletonLoader />}>
       <MetricsDashboard />
